fd17d5a4527774ccd6e16b08c9e29a3b129dea52
Peter Ringering <peteman316@gmail.com>
2025-06-07 13:31:28 -0600

    Working

---
+++ RingSoft.TaskLogix.DataAccess/Model/TlTask.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace RingSoft.TaskLogix.DataAccess.Model
+{
+    public class TlTask
+    {
+        [Required]
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Subject { get; set; }
+    }
+}
