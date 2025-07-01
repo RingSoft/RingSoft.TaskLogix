4834b6e330b476f6f4b23776cf1dbd0e340a1735
Peter Ringering <peteman316@gmail.com>
2025-07-01 17:15:55 -0600

    Working

--- RingSoft.TaskLogix.Library/Processors/TaskRecurMonthlyProcessor.cs
+++ RingSoft.TaskLogix.Library/Processors/TaskRecurMonthlyProcessor.cs
@@ -31,6 +31,7 @@ namespace RingSoft.TaskLogix.Library.Processors
                     TaskProcessor.StartDate = GetDayXOfEvery(TaskProcessor.StartDate, OfEveryYMonths);
                     break;
                 case MonthlyRecurTypes.XthWeekdayOfEveryYMonths:
+                    TaskProcessor.StartDate = GetNthWeekdayOfEveryMonth(TaskProcessor.StartDate, OfEveryWeekTypeMonths);
                     break;
                 case MonthlyRecurTypes.RegenerateXMonthsAfterCompleted:
                     break;
@@ -57,6 +58,107 @@ namespace RingSoft.TaskLogix.Library.Processors
             }
 
             return new DateTime(startDate.Year, startDate.Month, newDay);
+        }
+
+        internal DateTime GetNthWeekdayOfEveryMonth(DateTime startDate, int addMonths)
+        {
+            startDate = new DateTime(startDate.Year, startDate.Month, 1);
+            startDate = startDate.AddMonths(addMonths);
+            var result = startDate;
+            switch (WeekType)
+            {
+                case WeekTypes.First:
+                case WeekTypes.Second:
+                case WeekTypes.Third:
+                case WeekTypes.Fourth:
+                    switch (DayType)
+                    {
+                        case DayTypes.Day:
+                            result = GetNthDayTypeDay(startDate);
+                            break;
+                        case DayTypes.Weekday:
+                            result = GetNthWeekTypeWeekDay(startDate);
+                            break;
+                        case DayTypes.WeekendDay:
+                            result = GetNthWeekTypeWeekendDay(startDate);
+                            break;
+                        case DayTypes.Sunday:
+                        case DayTypes.Monday:
+                        case DayTypes.Tuesday:
+                        case DayTypes.Wednesday:
+                        case DayTypes.Thursday:
+                        case DayTypes.Friday:
+                        case DayTypes.Saturday:
+                            result = GetNthWeekDayDay(startDate);
+                            break;
+                        default:
+                            throw new ArgumentOutOfRangeException();
+                    }
+                    break;
+                case WeekTypes.Last:
+                    switch (DayType)
+                    {
+                        case DayTypes.Day:
+                            result = new DateTime(startDate.Year, startDate.Month, startDate.GetLastDayOfMonth());
+                            break;
+                        case DayTypes.Weekday:
+                            result = GetLastWeekTypeWeekDay(startDate);
+                            break;
+                        case DayTypes.WeekendDay:
+                            result = GetLastWeeklyTypeWeekendDay(startDate);
+                            break;
+                        case DayTypes.Sunday:
+                        case DayTypes.Monday:
+                        case DayTypes.Tuesday:
+                        case DayTypes.Wednesday:
+                        case DayTypes.Thursday:
+                        case DayTypes.Friday:
+                        case DayTypes.Saturday:
+                            result = GetLastWeekDayDay(startDate);
+                            break;
+                        default:
+                            throw new ArgumentOutOfRangeException();
+                    }
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            return result;
+        }
+
+        private DateTime GetLastWeekTypeWeekDay(DateTime startDate)
+        {
+            throw new Exception();
+        }
+
+        private DateTime GetLastWeeklyTypeWeekendDay(DateTime startDate)
+        {
+            throw new Exception();
+        }
+
+        private DateTime GetLastWeekDayDay(DateTime startDate)
+        {
+            throw new Exception();
+        }
+
+        private DateTime GetNthDayTypeDay(DateTime startDate)
+        {
+            throw new Exception();
+        }
+        private DateTime GetNthWeekDayDay(DateTime startDate)
+        {
+            throw new Exception();
+        }
+
+        private DateTime GetNthWeekTypeWeekDay(DateTime startDate)
+        {
+            throw new Exception();
+        }
+
+        private DateTime GetNthWeekTypeWeekendDay(DateTime startDate)
+        {
+            throw new Exception();
         }
     }
 }
