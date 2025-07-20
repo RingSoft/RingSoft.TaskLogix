4b6e81a634832e2438b189b66f4134d9b55f5e1a
Peter Ringering <peteman316@gmail.com>
2025-07-19 20:03:39 -0600

    Working

--- RingSoft.TaskLogix.App/TaskMaintenance/TaskMaintenanceUserControl.xaml.cs
+++ RingSoft.TaskLogix.App/TaskMaintenance/TaskMaintenanceUserControl.xaml.cs
@@ -1,75 +1,30 @@
-﻿using RingSoft.App.Controls;
-using RingSoft.DataEntryControls.WPF;
-using RingSoft.DbLookup;
-using RingSoft.DbLookup.Controls.WPF;
-using RingSoft.DbMaintenance;
-using RingSoft.TaskLogix.Library;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
 using System.Windows.Input;
-using RingSoft.TaskLogix.Library.ViewModels;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using RingSoft.DbLookup.Controls.WPF;
+using RingSoft.DbMaintenance;
 
 namespace RingSoft.TaskLogix.App.TaskMaintenance
 {
-    public class TaskHeaderControl : DbMaintenanceCustomPanel
-    {
-        public DbMaintenanceButton MarkCompleteButton { get; set; }
-
-        public DbMaintenanceButton RecurrenceButton { get; set; }
-
-        static TaskHeaderControl()
-        {
-            DefaultStyleKeyProperty.OverrideMetadata(typeof(TaskHeaderControl)
-                , new FrameworkPropertyMetadata(typeof(TaskHeaderControl)));
-        }
-
-        public override void OnApplyTemplate()
-        {
-            MarkCompleteButton = GetTemplateChild(nameof(MarkCompleteButton)) as DbMaintenanceButton;
-            RecurrenceButton = GetTemplateChild(nameof(RecurrenceButton)) as DbMaintenanceButton;
-
-            base.OnApplyTemplate();
-        }
-    }
     /// <summary>
     /// Interaction logic for TaskMaintenanceUserControl.xaml
     /// </summary>
-    public partial class TaskMaintenanceUserControl : ITaskMaintenanceView
+    public partial class TaskMaintenanceUserControl
     {
         public TaskMaintenanceUserControl()
         {
             InitializeComponent();
-            RegisterFormKeyControl(SubjectControl);
-
-            TopHeaderControl.Loaded += (sender, args) =>
-            {
-                if (TopHeaderControl.CustomPanel is TaskHeaderControl taskHeaderControl)
-                {
-                    taskHeaderControl.MarkCompleteButton.Command =
-                        LocalViewModel.MarkCompleteCommand;
-                    taskHeaderControl.RecurrenceButton.Command =
-                        LocalViewModel.RecurrenceCommand;
-
-                    taskHeaderControl.MarkCompleteButton.ToolTip.HeaderText = "Mark Complete (Ctrl + T, Ctrl + M)";
-                    taskHeaderControl.MarkCompleteButton.ToolTip.DescriptionText = "Mark this task as complete.";
-
-                    taskHeaderControl.RecurrenceButton.ToolTip.HeaderText = "Setup Recurrence (Ctrl + T, Ctrl + R)";
-                    taskHeaderControl.RecurrenceButton.ToolTip.DescriptionText =
-                        "Setup recurrence for this task.";
-                }
-            };
-
-            var hotKey = new HotKey(LocalViewModel.MarkCompleteCommand);
-            hotKey.AddKey(Key.T);
-            hotKey.AddKey(Key.M);
-            AddHotKey(hotKey);
-
-            hotKey = new HotKey(LocalViewModel.RecurrenceCommand);
-            hotKey.AddKey(Key.T);
-            hotKey.AddKey(Key.R);
-            AddHotKey(hotKey);
-
-            LocalViewModel.Init(this);
         }
 
         protected override DbMaintenanceViewModelBase OnGetViewModel()
@@ -89,14 +44,7 @@ namespace RingSoft.TaskLogix.App.TaskMaintenance
 
         protected override string GetTitle()
         {
-            return "Tasks";
-        }
-
-        public bool ShowTaskRecurrenceWindow()
-        {
-            var win = new TaskRecurWindow(LocalViewModel.TaskProcessor);
-            LookupControlsGlobals.WindowRegistry.ShowDialog(win);
-            return win.DialogResult;
+            return "Task";
         }
     }
 }
---
+++ RingSoft.TaskLogix.Library/ViewModels/TaskRecurWindowViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using RingSoft.TaskLogix.DataAccess.Model;
+using RingSoft.TaskLogix.Library.Processors;
+
+namespace RingSoft.TaskLogix.Library.ViewModels
+{
+    public interface IRecurWindowView
+    {
+        TaskProcessor TaskProcessor { get; set; }
+        void UpdateRecurType();
+    }
+    public class TaskRecurWindowViewModel : INotifyPropertyChanged
+    {
+        private TaskRecurTypes _recurType;
+
+        public TaskRecurTypes RecurType
+        {
+            get { return _recurType; }
+            set
+            {
+                if (_recurType == value)
+                {
+                    return;
+                }
+
+                _recurType = value;
+                OnPropertyChanged();
+                View.UpdateRecurType();
+            }
+        }
+
+        public IRecurWindowView View { get; private set; }
+
+        public void Init(IRecurWindowView view)
+        {
+            View = view;
+
+            var recurType = view.TaskProcessor.RecurType;
+            if (recurType == TaskRecurTypes.None)
+            {
+                recurType = TaskRecurTypes.Weekly;
+            }
+            RecurType = recurType;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
