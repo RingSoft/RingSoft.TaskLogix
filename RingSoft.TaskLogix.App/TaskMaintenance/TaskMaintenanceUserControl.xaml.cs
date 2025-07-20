c2139b9bebb48fa9952c12b178a92486d460bcc0
Peter Ringering <peteman316@gmail.com>
2025-07-20 17:04:05 -0600

    Working

--- RingSoft.TaskLogix.App/TaskMaintenance/TaskMaintenanceUserControl.xaml.cs
+++ RingSoft.TaskLogix.App/TaskMaintenance/TaskMaintenanceUserControl.xaml.cs
@@ -14,17 +14,19 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using RingSoft.DbLookup.Controls.WPF;
 using RingSoft.DbMaintenance;
+using RingSoft.TaskLogix.Library.ViewModels;
 
 namespace RingSoft.TaskLogix.App.TaskMaintenance
 {
     /// <summary>
     /// Interaction logic for TaskMaintenanceUserControl.xaml
     /// </summary>
-    public partial class TaskMaintenanceUserControl
+    public partial class TaskMaintenanceUserControl : ITaskMaintenanceView
     {
         public TaskMaintenanceUserControl()
         {
             InitializeComponent();
+            RegisterFormKeyControl(SubjectControl);
         }
 
         protected override DbMaintenanceViewModelBase OnGetViewModel()
@@ -45,6 +47,11 @@ namespace RingSoft.TaskLogix.App.TaskMaintenance
         protected override string GetTitle()
         {
             return "Task";
+        }
+
+        public bool ShowTaskRecurrenceWindow()
+        {
+            throw new NotImplementedException();
         }
     }
 }
