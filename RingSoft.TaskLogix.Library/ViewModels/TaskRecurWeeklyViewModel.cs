23346ce2267e489ae89ac5fd2cd5048acf6da2a3
Peter Ringering <peteman316@gmail.com>
2025-07-26 19:51:20 -0600

    Working

--- RingSoft.TaskLogix.Library/ViewModels/TaskRecurWeeklyViewModel.cs
+++ RingSoft.TaskLogix.Library/ViewModels/TaskRecurWeeklyViewModel.cs
@@ -1,9 +1,78 @@
-﻿using RingSoft.TaskLogix.Library.Processors;
+﻿using RingSoft.DataEntryControls.Engine;
+using RingSoft.TaskLogix.DataAccess.Model;
+using RingSoft.TaskLogix.Library.Processors;
 
 namespace RingSoft.TaskLogix.Library.ViewModels
 {
     public class TaskRecurWeeklyViewModel : TaskRecurViewModelBase
     {
+        private WeeklyRecurTypes _recurType;
+
+        public WeeklyRecurTypes RecurType
+        {
+            get { return _recurType; }
+            set
+            {
+                if (_recurType == value)
+                {
+                    return;
+                }
+                _recurType = value;
+                SetEnabled();
+                OnPropertyChanged();
+            }
+        }
+
+        private int _recurWeeks;
+
+        public int RecurWeeks
+        {
+            get { return _recurWeeks; }
+            set
+            {
+                if (_recurWeeks == value)
+                    return;
+
+                _recurWeeks = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private int _regenWeeksAfterCompleted;
+
+        public int RegenWeeksAfterCompleted
+        {
+            get { return _regenWeeksAfterCompleted; }
+            set
+            {
+                if (_regenWeeksAfterCompleted == value)
+                    return;
+
+                _regenWeeksAfterCompleted = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public UiCommand RecurWeeksUiCommand { get; }
+
+        public UiCommand RegenWeeksUiCommand { get; set; }
+
+        public TaskRecurWeeklyViewModel()
+        {
+            RecurWeeksUiCommand = new UiCommand();
+            RegenWeeksUiCommand = new UiCommand();
+
+            RecurType = WeeklyRecurTypes.EveryXWeeks;
+            RecurWeeks = 1;
+            RegenWeeksAfterCompleted = 1;
+
+        }
+
+        public void SetEnabled()
+        {
+
+        }
+
         public override void LoadFromTaskProcessor(TaskProcessor taskProcessor)
         {
             
