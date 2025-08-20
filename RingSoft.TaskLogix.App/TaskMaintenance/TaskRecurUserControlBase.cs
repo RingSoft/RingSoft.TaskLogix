3897864f6989990f541a36a5c556b1fe19ce3529
Peter Ringering <peteman316@gmail.com>
2025-08-19 20:14:04 -0600

    Working

--- RingSoft.TaskLogix.App/TaskMaintenance/TaskRecurUserControlBase.cs
+++ RingSoft.TaskLogix.App/TaskMaintenance/TaskRecurUserControlBase.cs
@@ -1,86 +1,10 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
-using RingSoft.DataEntryControls.WPF;
 using RingSoft.TaskLogix.Library.ViewModels;
 using System.Windows.Input;
-using RingSoft.TaskLogix.SqlServer.Migrations;
 
 namespace RingSoft.TaskLogix.App.TaskMaintenance
 {
-    public class RecurControlRadioButton : RadioButton
-    {
-        public TaskRecurWindow RecurWindow { get; private set; }
-
-        public static readonly DependencyProperty NextDownRadioButtonProperty =
-            DependencyProperty.Register(nameof(NextDownRadioButton), typeof(RecurControlRadioButton), typeof(RecurControlRadioButton),
-                new FrameworkPropertyMetadata(NextDownRadioButtonChangedCallback));
-
-        /// <summary>
-        /// Gets or sets the UI label.  This is a bind-able property.
-        /// </summary>
-        /// <value>The UI label.</value>
-        public RecurControlRadioButton NextDownRadioButton
-        {
-            get { return (RecurControlRadioButton)GetValue(NextDownRadioButtonProperty); }
-            set { SetValue(NextDownRadioButtonProperty, value); }
-        }
-
-        /// <summary>
-        /// UIs the label changed callback.
-        /// </summary>
-        /// <param name="obj">The object.</param>
-        /// <param name="args">The <see cref="DependencyPropertyChangedEventArgs" /> instance containing the event data.</param>
-        private static void NextDownRadioButtonChangedCallback(DependencyObject obj,
-            DependencyPropertyChangedEventArgs args)
-        {
-            var recurRadioButton = (RecurControlRadioButton)obj;
-            if (recurRadioButton != null)
-            {
-                
-            }
-        }
-
-        public RecurControlRadioButton NextUpRadioButton { get; private set; }
-
-        public RecurControlRadioButton()
-        {
-            Loaded += (sender, args) =>
-            {
-                RecurWindow = this.GetParentOfType<TaskRecurWindow>();
-
-                if (NextDownRadioButton != null)
-                {
-                    NextDownRadioButton.NextUpRadioButton = this;
-                }
-            };
-        }
-        protected override void OnKeyDown(KeyEventArgs e)
-        {
-            if (e.Key == Key.Tab)
-            {
-            }
-
-            if (e.Key == Key.Down)
-            {
-                if (NextDownRadioButton != null)
-                {
-                    NextDownRadioButton.Focus();
-                    e.Handled |= true;
-                }
-            }
-
-            if (e.Key == Key.Up)
-            {
-                if (NextUpRadioButton != null)
-                {
-                    NextUpRadioButton.Focus();
-                    e.Handled |= true;
-                }
-            }
-
-            base.OnKeyDown(e);
-        }
-    }
     public abstract class TaskRecurUserControlBase : UserControl
     {
         public abstract TaskRecurViewModelBase GetRecurViewModel();
---
+++ RingSoft.TaskLogix.App/TlControlRadioButton.cs
@@ -0,0 +1,77 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace RingSoft.TaskLogix.App
+{
+    public class TlControlRadioButton : RadioButton
+    {
+        public static readonly DependencyProperty NextDownRadioButtonProperty =
+            DependencyProperty.Register(nameof(NextDownRadioButton), typeof(TlControlRadioButton), typeof(TlControlRadioButton),
+                new FrameworkPropertyMetadata(NextDownRadioButtonChangedCallback));
+
+        /// <summary>
+        /// Gets or sets the UI label.  This is a bind-able property.
+        /// </summary>
+        /// <value>The UI label.</value>
+        public TlControlRadioButton NextDownRadioButton
+        {
+            get { return (TlControlRadioButton)GetValue(NextDownRadioButtonProperty); }
+            set { SetValue(NextDownRadioButtonProperty, value); }
+        }
+
+        /// <summary>
+        /// UIs the label changed callback.
+        /// </summary>
+        /// <param name="obj">The object.</param>
+        /// <param name="args">The <see cref="DependencyPropertyChangedEventArgs" /> instance containing the event data.</param>
+        private static void NextDownRadioButtonChangedCallback(DependencyObject obj,
+            DependencyPropertyChangedEventArgs args)
+        {
+            var recurRadioButton = (TlControlRadioButton)obj;
+            if (recurRadioButton != null)
+            {
+
+            }
+        }
+
+        public TlControlRadioButton NextUpRadioButton { get; private set; }
+
+        public TlControlRadioButton()
+        {
+            Loaded += (sender, args) =>
+            {
+                if (NextDownRadioButton != null)
+                {
+                    NextDownRadioButton.NextUpRadioButton = this;
+                }
+            };
+        }
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            if (e.Key == Key.Tab)
+            {
+            }
+
+            if (e.Key == Key.Down)
+            {
+                if (NextDownRadioButton != null)
+                {
+                    NextDownRadioButton.Focus();
+                    e.Handled |= true;
+                }
+            }
+
+            if (e.Key == Key.Up)
+            {
+                if (NextUpRadioButton != null)
+                {
+                    NextUpRadioButton.Focus();
+                    e.Handled |= true;
+                }
+            }
+
+            base.OnKeyDown(e);
+        }
+    }
+}
