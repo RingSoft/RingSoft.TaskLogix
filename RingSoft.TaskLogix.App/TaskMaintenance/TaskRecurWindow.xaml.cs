64631cdc4f55c777a91525c8c86336a8591b11f5
Peter Ringering <peteman316@gmail.com>
2025-08-31 16:23:12 -0600

    Working

--- RingSoft.TaskLogix.App/TaskMaintenance/TaskRecurWindow.xaml.cs
+++ RingSoft.TaskLogix.App/TaskMaintenance/TaskRecurWindow.xaml.cs
@@ -76,6 +76,7 @@ namespace RingSoft.TaskLogix.App.TaskMaintenance
             {
                 RecurPanel.Children.Add(ActiveRecurUserControl);
                 LocalViewModel.ActiveRecurViewModel = ActiveRecurUserControl.GetRecurViewModel();
+                LocalViewModel.ActiveRecurViewModel.Init();
             }
             RecurPanel.UpdateLayout();
         }
